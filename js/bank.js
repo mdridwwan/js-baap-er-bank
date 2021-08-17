48cfb45942a95fdda78c436cf74d45e4c5f733cb
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2021-08-17 22:39:39 +0600

    last update

--- js/bank.js
+++ js/bank.js
@@ -9,39 +9,51 @@ function dobuleValue(num) {
 }
 //view function
 function doubleView(text, num) {
+    //  debugger;
     const textView = document.getElementById(text);
     const VeiwText = textView.innerText;
     const VeiwNum = parseFloat(VeiwText);
     const viewTotal = VeiwNum + num;
     textView.innerText = viewTotal;
 
-    //return VeiwNum;
+}
+//current balance
+function currentbalance() {
+    const balanceView = document.getElementById('balance-view');
+    const balanceViewNum = balanceView.innerText;
+    const totalBalance = parseFloat(balanceViewNum);
+    return totalBalance;
+}
+//update Balance
+function updateBalance(allNum, isAdd) {
+    const balanceView = document.getElementById('balance-view');
+    const balanceViewNum = balanceView.innerText;
+    if (isAdd == true) {
+        const totalBalance = allNum + parseFloat(balanceViewNum);
+        balanceView.innerText = totalBalance;
+    } else {
+        const totalBalance = parseFloat(balanceViewNum) - allNum;
+        balanceView.innerText = totalBalance;
+    }
 }
 
 //Deposit
 document.getElementById('deposit-btn').addEventListener('click', function() {
-    //input
+
     const depositNum = dobuleValue('deposit-input');
-    //view
-    doubleView('deposit-view', depositNum);
-    //Total Balance
-    const balanceView = document.getElementById('balance-view');
-    const balanceViewNum = balanceView.innerText;
-    const totalBalance = depositNum + parseFloat(balanceViewNum);
-    balanceView.innerText = totalBalance;
+    if (depositNum > 0) {
+        doubleView('deposit-view', depositNum);
+        updateBalance(depositNum, true);
+    }
 })
 
 // Withdraw
 document.getElementById('withdraw-btn').addEventListener('click', function() {
-    //input
-    const withdrawNum = dobuleValue('withdraw-input');
-    //view
-    doubleView('withdraw-view', withdrawNum)
 
-    //Balance
-    const balanceView = document.getElementById('balance-view');
-    const balanceViewNum = balanceView.innerText;
-    const totalBalance = parseFloat(balanceViewNum) - withdrawNum;
-    balanceView.innerText = totalBalance;
+    const withdrawNum = dobuleValue('withdraw-input');
+    if (withdrawNum > 0 && currentbalance() >= withdrawNum) {
+        doubleView('withdraw-view', withdrawNum)
+        updateBalance(withdrawNum, false);
+    }
 
 })
